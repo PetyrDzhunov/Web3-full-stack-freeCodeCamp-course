6d95556068bfb05c91a33d2e957072dbc85fa052
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-01 20:35:45 +0300

    add verify function , create an account in etherscan to add plugin in the hardhat for verifying

--- hardhat/hardhat-simple-storage/scripts/deploy.js
+++ hardhat/hardhat-simple-storage/scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,6 +8,28 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.deployed();
     console.log(`Deployed contract to ${simpleStorage.address}`);
+    // ONLY verify if we are on the rinkeby(chainID=4) test network
+    // and we have etherscan api key in the .env file
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6); // wait 6 blocks to be mined and then run verification proccess
+        await verify(simpleStorage.address, []);
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Veryfying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (err) {
+        if (err.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(err);
+        }
+    }
 }
 
 main()
