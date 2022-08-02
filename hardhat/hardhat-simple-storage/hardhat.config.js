239d7ac559e6419672fb3d7f15f355b0de2a3a8f
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-02 19:28:05 +0300

    add hardhat-gas-reporter to check gas fees and prevent hardhat configuration problems with .env

--- hardhat/hardhat-simple-storage/hardhat.config.js
+++ hardhat/hardhat-simple-storage/hardhat.config.js
@@ -2,11 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -23,5 +25,12 @@ module.exports = {
     solidity: "0.8.9",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
 };
