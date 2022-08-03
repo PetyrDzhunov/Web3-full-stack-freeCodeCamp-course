76b7dc0cdf51b6d1a8b4a866a0794d3bd92106c3
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-03 23:12:05 +0300

    deploy mocks or real contract on local hardhat network depending on the network

--- hardhat/hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ hardhat/hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -1,8 +1,33 @@
 //hardhat-deployer gives us the hre as a param when we run
 // deploy so we are destructuring to get only what we need
+const {
+  networkConfig,
+  developmentChains,
+} = require('../helper-hardhat-config');
+const { network } = require('hardhat');
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
+  const { deploy, log, get } = deployments;
   const { deployer } = await getNamedAccounts(); // from hardhat.config.js
-  const chainId = network.config.chainID;
+
+  let ethUsdPriceFeedAddress;
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await get('MockV3Aggregator');
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainID]['ethUsdPriceFeed'];
+  }
+  //if the contract doesn't exist , we depoy a minimal version of  contract for our local testing
+
+  //when going for localhost or hardhat network we want to use a mock
+  // what happens when we want to change chains? - they have
+  // different addresses because they are different contracts on different chains
+  const fundMe = await deploy('FundMe', {
+    from: deployer,
+    args: [ethUsdPriceFeedAddress], // price feed address,
+    log: true,
+  });
+  log('-/-/-/-/-/-/-/-/-/-/-/-/-/-/-//-/-/-/-/-/-/-/-');
 };
+
+module.exports.tags = ['all', 'fundme'];
--- hardhat/hardhat-fund-me/hardhat.config.js
+++ hardhat/hardhat-fund-me/hardhat.config.js
@@ -3,7 +3,10 @@ require('hardhat-deploy');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.7',
+  // solidity: '0.8.7',
+  solidity: {
+    compilers: [{ version: '0.8.8' }, { version: '0.6.6' }],
+  },
   defaultNetwork: 'hardhat',
   namedAccounts: {
     deployer: {
