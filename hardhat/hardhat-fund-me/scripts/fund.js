8f68a5114bea57e5501680b29e7d8448c969e723
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-06 14:07:48 +0300

    create scripts that interract with our contract in local dev environment using the hardhat commands

---
+++ hardhat/hardhat-fund-me/scripts/fund.js
@@ -0,0 +1,24 @@
+// script to interract with our contrat
+// just to fund one of our contracts
+
+const { getNamedAccounts, ethers } = require("hardhat");
+
+const main = async () => {
+  const { deployer } = await getNamedAccounts();
+  const fundMe = await ethers.getContract("FundMe", deployer);
+
+  console.log("Funding Contract....");
+  const transactionResponse = await fundMe.fund({
+    value: ethers.utils.parseEther("0.1"),
+  });
+
+  await transactionResponse.wait(1);
+  console.log("Funded");
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
