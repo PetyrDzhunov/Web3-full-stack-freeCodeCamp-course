4da0e7e5b732d2ed477814ea95421d97b3aaf396
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-10 14:43:14 +0300

    add all the required arguments for the constructor

--- hardhat/hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
+++ hardhat/hardhat-smartcontract-lottery/deploy/01-deploy-raffle.js
@@ -4,24 +4,45 @@ const {
   networkConfig,
 } = require('../helper-hardhat-config');
 
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther('2');
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
-  let vrfCoordinatorV2Address;
+  let vrfCoordinatorV2Address, subscriptionId;
 
   if (developmentChains.includes(network.name)) {
     const vrfCoordinatorV2Mock = await ethers.getContract(
       'VRFCoordinatorV2Mock',
     );
     vrfCoordinatorV2Address = vrfCoordinatorV2Mock;
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait(1); // in this receipt there's an event that is emitted with subscription that we can get
+    subscriptionId = transactionReceipt.events[0].args.subId;
+    //Fund the subscription
+    // Usually you'd need the link token on a real network
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT,
+    );
   } else {
     vrfCoordinatorV2Address = networkConfig[chainId]['vrfCoordinatorV2'];
+    subscriptionId = networkConfig[chainId]['subscriptionId'];
   }
-  const entanceFee = networkConfig[chainId]['entranceFee'];
+  const entranceFee = networkConfig[chainId]['entranceFee'];
   const gasLane = networkConfig[chainId]['gasLane'];
+  const callbackGasLimit = networkConfig[chainId]['callbackGasLimit'];
+  const interval = networkConfig[chainId]['interval'];
 
-  const args = [vrfCoordinatorV2Address, entranceFee, gasLane];
+  const args = [
+    vrfCoordinatorV2Address,
+    entranceFee,
+    gasLane,
+    subscriptionId,
+    callbackGasLimit,
+    interval,
+  ];
 
   const raffle = await deploy('Raffle', {
     from: deployer,
