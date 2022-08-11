eabfe02175182169e7823eb5f063d5d26cae7e27
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-11 17:57:04 +0300

    basic useEffects to keep state between refresh with localstorage

--- hardhat/nextjs-smartcontract-lottery/components/ManualHeader.js
+++ hardhat/nextjs-smartcontract-lottery/components/ManualHeader.js
@@ -1,17 +1,44 @@
+import { useEffect } from 'react';
 import { useMoralis } from 'react-moralis';
 
 export default function ManualHeader() {
-  const { enableWeb3, account } = useMoralis();
+  const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } =
+    useMoralis();
 
   const connect = async () => {
     await enableWeb3();
-    console.log(account);
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem('connected', 'injected');
+    }
   };
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      console.log(`Account changed to ${account}`);
+      if (account == null) {
+        window.localStorage.removeItem('connected');
+        deactivateWeb3();
+        console.log(`Null account found`);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isWeb3Enabled) return;
+    if (typeof window !== 'undefined') {
+      if (window.localStorage.getItem('connected')) {
+        enableWeb3();
+      }
+    }
+  }, []);
 
   return (
     <div>
       {account ? (
-        <div>Conncted!</div>
+        <div>
+          Conncted to {account.slice(0, 6)}...
+          {account.slice(account.length - 4)}
+        </div>
       ) : (
         <button onClick={connect}>Connect</button>
       )}
