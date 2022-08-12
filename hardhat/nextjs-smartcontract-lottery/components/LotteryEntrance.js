7ea352c159b06657c14b52f4bec9d013a3d1c6a2
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-12 14:07:59 +0300

    basic way to connect contract to front end and use getEntranceFee function from it

--- hardhat/nextjs-smartcontract-lottery/components/LotteryEntrance.js
+++ hardhat/nextjs-smartcontract-lottery/components/LotteryEntrance.js
@@ -1,15 +1,41 @@
 // have a function to enter the lottery
 
 import { useWeb3Contract } from 'react-moralis';
+import { contractAddresses, abi } from '../constants';
+import { useMoralis } from 'react-moralis';
+import { useEffect } from 'react';
 
 export default function LotteryEntrance() {
-  // const {runContractFunction: enterRaffle} = useWeb3Contract({
-  // 	abi : '',
-  // 	contractAddress : '',
-  // 	functionName : '',
-  // 	params : {},
-  // 	msgValue
-  // })
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis(); // hex number of the chainId
+  const chainId = parseInt(chainIdHex);
+  console.log(chainId);
+  const raffleAddress =
+    chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+
+  // const { runContractFunction: enterRaffle } = useWeb3Contract({
+  //   abi: abi,
+  //   contractAddress: raffleAddress,
+  //   functionName: 'enterRaffle',
+  //   params: {},
+  //   msgValue: '', //
+  // });
+
+  const { runContractFunction: getEntranceFee } = useWeb3Contract({
+    abi: abi,
+    contractAddress: raffleAddress,
+    functionName: 'getEntranceFee',
+    params: {},
+  });
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      async function updateUI() {
+        const entranceFeeFromContract = await getEntranceFee();
+        console.log(entranceFeeFromContract);
+      }
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
 
   return <div>Hi from Lottery Entrance</div>;
 }
