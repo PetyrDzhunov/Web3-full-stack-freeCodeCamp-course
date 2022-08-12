e3f20931e56a7b69faf1040e786f7587d1658128
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-12 16:41:10 +0300

    create a script for getting the recent winner

--- hardhat/hardhat-smartcontract-lottery/scripts/mockOffchain.js
+++ hardhat/hardhat-smartcontract-lottery/scripts/mockOffchain.js
@@ -4,12 +4,14 @@ async function mockKeepers() {
   const raffle = await ethers.getContract('Raffle');
   const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(''));
   const { upkeepNeeded } = await raffle.callStatic.checkUpkeep(checkData);
+  console.log(checkData);
+  console.log(upkeepNeeded);
   if (upkeepNeeded) {
     const tx = await raffle.performUpkeep(checkData);
     const txReceipt = await tx.wait(1);
     const requestId = txReceipt.events[1].args.requestId;
     console.log(`Performed upkeep with RequestId: ${requestId}`);
-    if (network.config.chainId == 31337) {
+    if (network.config.url == 'http://127.0.0.1:8545') {
       await mockVrf(requestId, raffle);
     }
   } else {
