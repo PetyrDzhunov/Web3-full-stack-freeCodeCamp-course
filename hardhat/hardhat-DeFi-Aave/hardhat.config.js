2f0b729b265c65b351f3644dbfe59fe532b0cb59
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-14 15:28:14 +0300

    forking mainnet and why should we do it - pros/cons

--- hardhat/hardhat-DeFi-Aave/hardhat.config.js
+++ hardhat/hardhat-DeFi-Aave/hardhat.config.js
@@ -10,13 +10,16 @@ const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 31337,
-      blockConfirmations: 1,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
     },
     rinkeby: {
       chainId: 4,
