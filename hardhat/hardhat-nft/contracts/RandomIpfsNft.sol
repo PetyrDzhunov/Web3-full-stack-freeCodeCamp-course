cef8ed542cbdff468b3a9b7293c63f0be82dbca7
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-16 12:45:36 +0300

    add breed enum and a function to get it depending on the random number we get from the chainlink vrf

--- hardhat/hardhat-nft/contracts/RandomIpfsNft.sol
+++ hardhat/hardhat-nft/contracts/RandomIpfsNft.sol
@@ -6,6 +6,8 @@ import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+error RandomIpfsNft__RangeOutOfBounds;
+
 /**
 	when we mint an NFT, we will trigger a Chainlink VRF call to get us a random number
 	using that number, we will get a random NFT
@@ -26,6 +28,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 		//NFT variables
 		uint256 public s_tokenCounter;
+		uint256 internal constant MAX_CHANCE_VALUE = 100;
+
+		//Type Declaration
+		enum Breed {
+			PUG,
+			SHIBA_INU,
+			ST.BERNARD
+		};
 
 contract RandomIpfsNft is VRFConsumerBaseV2,ERC721 {
 
@@ -34,8 +44,9 @@ contract RandomIpfsNft is VRFConsumerBaseV2,ERC721 {
 	 bytes32 gasLane,
 	 uint32 callbackGasLimit
 	)
-	ERC721("Dogie", "DOG")
-	 	VRFConsumerBaseV2(vrfCoordinatorV2)  {
+
+	 	VRFConsumerBaseV2(vrfCoordinatorV2) 
+		ERC721("Random IPFS NFT", "RIN")  {
 		i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
 	  i_subscriptionId = subscriptionId;
 		i_gasLane = gasLane;
@@ -59,10 +70,32 @@ contract RandomIpfsNft is VRFConsumerBaseV2,ERC721 {
 		// mint this random NFT(dog) for this user and save it to his account/address
 		uint256 newTokenId = s_tokenCounter;
 		_safeMint(dogOwner,newTokenId);
+		//What does this token look like?
+		uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;// between 0-99
+
+		getBreedFromModdedRng()
 		s_tokenCounter = s_tokenCounter + 1;
 	}
 
-	function tokenURI(uint256) public {
+	function getBreedFromModdedRng(uint256 moddedRng) public pure return(Breed) {
+		uint256 cumulativeSum = 0;
+		uint256[3] memory chanceArray = getChanceArray();
+		for(uint256 i = 0; i<chanceArray.length; i++) {
+			if(moddedRng >= cumulativeSum && moddedRng < cumulativeSum + chanceArray[i]) {
+				return Breed(i);
+			};
+			cumulativeSum +=chanceArray[i];
+		}
+		revert RandomIpfsNft__RangeOutOfBounds();
+	}
+
+	function getChanceArray() public pure returns(uint256[3] memory) {
+		return [10,30,MAX_CHANCE_VALUE];
+		// pug - 10 % , shiba-inu 30%, st.bernard - 60% chance 
+	}	
+
+	function tokenURI(uint256) public view override returns(string memory) {
 
 	}
+
 }
