b2a1c2051a0c64c769617c25e25f7c624aed22b1
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-16 12:16:29 +0300

    create a mapping to save owners adresses to requstIds to use them as a address for the nft in the randomness function

--- hardhat/hardhat-nft/contracts/RandomIpfsNft.sol
+++ hardhat/hardhat-nft/contracts/RandomIpfsNft.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 /**
 	when we mint an NFT, we will trigger a Chainlink VRF call to get us a random number
@@ -20,14 +21,21 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 		uint16 private constant REQUEST_CONFIRMATIONS = 3;
 		uint32 private constant NUM_WORDS = 1;
 
-contract RandomIpfsNft is VRFConsumerBaseV2  {
+		//VRF Helpers
+		mapping(uint256 => address) public s_requestIdToSender; // so we know whoever call the requestNft function to be existent in fulfillRandomWords
+
+		//NFT variables
+		uint256 public s_tokenCounter;
+
+contract RandomIpfsNft is VRFConsumerBaseV2,ERC721 {
 
 	constructor(address vrfCoordinatorV2,
 	 uint64 subscriptionId,
 	 bytes32 gasLane,
 	 uint32 callbackGasLimit
 	)
-	 	VRFConsumerBaseV2(vrfCoordinatorV2) {
+	ERC721("Dogie", "DOG")
+	 	VRFConsumerBaseV2(vrfCoordinatorV2)  {
 		i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
 	  i_subscriptionId = subscriptionId;
 		i_gasLane = gasLane;
@@ -41,12 +49,17 @@ contract RandomIpfsNft is VRFConsumerBaseV2  {
 			REQUST_CONFIRMATIONS,
 			i_callbackGasLimit,
 			NUM_WORDS
-		)
+		);
+		s_requestIdToSender[requestId] = msg.sender;
 	}
 
 
 	function fulfillRandomWords(uint256 requestId,uint256[] memory randomWords) override internal {
-
+		address dogOwner = s_requestIdToSender[requestId];
+		// mint this random NFT(dog) for this user and save it to his account/address
+		uint256 newTokenId = s_tokenCounter;
+		_safeMint(dogOwner,newTokenId);
+		s_tokenCounter = s_tokenCounter + 1;
 	}
 
 	function tokenURI(uint256) public {
