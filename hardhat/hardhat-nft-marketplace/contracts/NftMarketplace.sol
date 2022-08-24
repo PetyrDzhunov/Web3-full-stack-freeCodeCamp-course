1c066b731d22b9d111dc90639e539feab2cf0383
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-24 22:10:11 +0300

    add a modifier to check if the token is not already listed

--- hardhat/hardhat-nft-marketplace/contracts/NftMarketplace.sol
+++ hardhat/hardhat-nft-marketplace/contracts/NftMarketplace.sol
@@ -6,29 +6,42 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 error NftMarketplace__PriceMustBeAboveZero();
 error NftMarketPlace__NotApprovedForMarketplace();
+error NftMarketplace__AlreadyListed(address nftAddress,uint256 tokenId);
 
 contract NftMarketplace {
 
 	struct Listing { 
 		uint256 price;
 		address seller;
-	};
+	}
 
-	event ItemListed {
+	event ItemListed (
 		address indexed seller,
 		address indexed nftAddress,
 		uint256 indexed tokenId,
 		uint256 price
-	}
+	);
 
 	// NFT Contract address -> NFT TokenID -> Listing
 	mapping(address => mapping(uint256 => Listing)) private s_listings;
 
 	//////////////////////
+	// Modifiers        //
+	//////////////////////
+	modifier notListed(address nftAddress,uint256 tokenId,address owner) {
+		Listing memory listing = s_listings[nftAddress][tokenId];
+		if(listing.price > 0) {
+			revert NftMarketplace__AlreadyListed(nftAddress,tokenId);
+		}
+		_;
+	}
+
+	//////////////////////
 	// Main Functions //
 	/////////////////////
 
-	function listItem(address nftAddress, uint256 token, uint256 price) external {
+	function listItem(address nftAddress, uint256 tokenId, uint256 price) external 
+	notListed(nftAddress,tokenId,msg.sender) {
 		if(price <=0) {
 			revert NftMarketplace__PriceMustBeAboveZero();
 		}
@@ -41,8 +54,9 @@ contract NftMarketplace {
 		 }
 		s_listings[nftAddress][tokenId] = Listing(price,msg.sender);
 		emit ItemListed(msg.sender,nftAddress,tokenId,price);
-
 	}
+
+	
 }
 
 
