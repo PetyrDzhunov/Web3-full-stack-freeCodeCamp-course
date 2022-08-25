bfbba9b52e9765ceb7e39d36c220d8cbe5938272
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-25 17:45:18 +0300

    create a script for deploying nft marketplace contract

--- hardhat/hardhat-nft-marketplace/contracts/NftMarketplace.sol
+++ hardhat/hardhat-nft-marketplace/contracts/NftMarketplace.sol
@@ -158,19 +158,8 @@ contract NftMarketplace is ReentrancyGuard {
 	function getListing(address nftAddress,uint256 tokenId) external view returns(Listing memory) {
 		return s_listings[nftAddress][tokenId];
 	}
-
+ 
 	function getProceeds(address seller) external view returns(uint256) {
 		return s_proceeds[seller];
 	}
-
-	
 }
-
-
-
-// 1. Create a decentralized NFT Marketplace
-//     1. `listItem`: List NFTs on the marketplace
-//     2. `buyItem`: Buy the NFTs
-//     3. `cancelItem`: Cancel a listing
-//     4. `updateListing`: Update price
-//     5. `withdrawProceeds`: Withdraw payment for my bought NFTs
