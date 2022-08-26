339c59794c37b9b4398d79ca82297280c20ebc7a
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-26 23:21:53 +0300

    finish the script about listening to the events and adding them to moralis DB

--- hardhat/nextjs-nft-marketplace-moralis/addEvent.js
+++ hardhat/nextjs-nft-marketplace-moralis/addEvent.js
@@ -1,27 +1,28 @@
 const Moralis = require('moralis/node');
 require('dotenv').config();
-const contractAddress = require('./constants/networkMapping.json');
+const contractAddresses = require('./constants/networkMapping.json');
 // Moralis understand a local chain is 1337
 let chainId = process.env.chainId || 31337;
 let moralisChainId = chainId == '31337' ? '1337' : chainId;
-const contractAddress = contractAddress[chainId]['NftMarketplace'][0]; // getting the most recently deployed nft marketplace contract
-const SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
-const APP_ID = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
-const MASTER_KEY = process.env.MASTER_KEY;
+const contractAddress = contractAddresses[chainId]['NftMarketplace'][0]; // getting the most recently deployed nft marketplace contract
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+const appId = process.env.NEXT_PUBLIC_APP_ID;
+const masterKey = process.env.MASTER_KEY;
 
 // We are indexing these events to be easier to query them.
 // We're saying moralis - hey listen to these events, whenever
 // you hear any of these 3 events stick all the stuff in the database so we can read from it.
 
 async function main() {
-  await Moralis.start({ SERVER_URL, APP_ID, MASTER_KEY });
+  await Moralis.start({ serverUrl, appId, masterKey });
   console.log('Working with contract address ' + contractAddress);
 
   let itemListedOptions = {
+    // Moralis understands a local chain is 1337
     chainId: moralisChainId,
     sync_historical: true,
-    address: contractAddress,
     topic: 'ItemListed(address,address,uint256,uint256)',
+    address: contractAddress,
     abi: {
       anonymous: false,
       inputs: [
@@ -58,8 +59,8 @@ async function main() {
 
   let itemBoughtOptions = {
     chainId: moralisChainId,
-    sync_historical: true,
     address: contractAddress,
+    sync_historical: true,
     topic: 'ItemBought(address,address,uint256,uint256)',
     abi: {
       anonymous: false,
@@ -97,9 +98,9 @@ async function main() {
 
   let itemCanceledOptions = {
     chainId: moralisChainId,
-    sync_historical: true,
     address: contractAddress,
     topic: 'ItemCanceled(address,address,uint256)',
+    sync_historical: true,
     abi: {
       anonymous: false,
       inputs: [
@@ -143,6 +144,16 @@ async function main() {
     itemCanceledOptions,
     { useMasterKey: true },
   );
+
+  if (
+    listedResponse.success &&
+    boughtResponse.success &&
+    canceledResponse.success
+  ) {
+    console.log('Sucess! Database Updated with watching events');
+  } else {
+    console.log('Something went wrong...');
+  }
 }
 
 main()
