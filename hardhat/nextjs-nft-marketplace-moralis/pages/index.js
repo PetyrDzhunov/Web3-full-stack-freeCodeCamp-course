ea2a316422e02e2c0bb089f97dba2a5b57d8d80c
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-26 14:02:51 +0300

    create a moralis server to use it for listening to events and saving it into DB, use frp for connecting the current blockchain network to the backend of moralis

--- hardhat/nextjs-nft-marketplace-moralis/pages/index.js
+++ hardhat/nextjs-nft-marketplace-moralis/pages/index.js
@@ -1,16 +1,10 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name='description' content='NFT Marketplace' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      Hi!
-    </div>
-  );
+  //We will index the events off-chain and then read from our database.
+  // Setup a server to listen for those events to be fire, and we will add them to a database to query.
+  // That is centralized with Moralis and decentralized with The Graph.
+
+  return <div className={styles.container}></div>;
 }
