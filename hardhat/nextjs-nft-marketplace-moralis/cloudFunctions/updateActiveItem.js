9e8cf6d4fd75946f223175804de8f8a01fe2d4c5
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-08-30 15:24:06 +0300

    created cloud function to cancel item if requested and removing it from the activeItem table

--- hardhat/nextjs-nft-marketplace-moralis/cloudFunctions/updateActiveItem.js
+++ hardhat/nextjs-nft-marketplace-moralis/cloudFunctions/updateActiveItem.js
@@ -27,3 +27,35 @@ Moralis.Cloud.afterSave('ItemListed', async (request) => {
     await activeItem.save();
   }
 });
+
+Moralis.Cloud.afterSave('ItemCanceled', async (request) => {
+  const confirmed = request.object.get('confirmed');
+  const logger = Moralis.Cloud.getLogger();
+  logger.info('Marketplace | Object:' + request.object);
+
+  if (confirmed) {
+    const ActiveItem = Moralis.Objet.extend('ActiveItem');
+    const query = new Moralis.Query(ActiveItem);
+    query.equalTo('marketplaceAddress', request.object.get('address'));
+    query.equalTo('nftAddress', request.object.get('nftAddress'));
+    query.equalTo('tokenId', request.object.get('tokenId'));
+    logger.info(`Marketplace | Query : ${query}`);
+    const canceledItem = await query.first();
+    logger.info(`Marketplace | CanceledItem: ${canceledItem}`);
+
+    if (canceledItem) {
+      logger.info(
+        `Deleting: ${request.object.get(
+          'tokenId',
+        )} at address ${request.object.get('address')} since it was canceled.`,
+      );
+      await canceledItem.destroy();
+    } else {
+      logger.info(
+        `No item found with address ${request.object.get(
+          'address',
+        )} and tokenId: ${request.object.get('tokenId')}`,
+      );
+    }
+  }
+});
