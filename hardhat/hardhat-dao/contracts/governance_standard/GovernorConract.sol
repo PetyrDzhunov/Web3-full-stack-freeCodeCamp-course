051f08eb2ee2a6f42446dcbf6ff3a91b983455a5
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-09-07 10:13:48 +0300

    install latest openzeppelin contracts to update

--- hardhat/hardhat-dao/contracts/governance_standard/GovernorConract.sol
+++ hardhat/hardhat-dao/contracts/governance_standard/GovernorConract.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
+ import "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
@@ -9,11 +10,17 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFractio
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
 contract GovernorContract is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
-    constructor(IVotes _token, TimelockController _timelock,uint256 _votingDelay,uint256 _votingPeriod,uint256 _quoromPercentage)
+    constructor(
+				IVotes _token,
+				TimelockController _timelock,
+				uint256 _votingDelay,
+				uint256 _votingPeriod,
+				uint256 _quoromPercentage
+		)
         Governor("GovernorContract")
         GovernorSettings(
-					votingDelay/* 1 block */, 
-					votingPeriod /* 45818 glocks = ~1 week */,
+					_votingDelay, 
+					_votingPeriod ,
 					 0
 					)
         GovernorVotes(_token)
