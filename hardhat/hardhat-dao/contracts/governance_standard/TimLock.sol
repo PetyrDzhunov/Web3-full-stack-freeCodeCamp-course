7344c0ccd902feeb26d28edb4e211a80d6f043cb
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2022-09-07 10:05:21 +0300

    create TimeLock contract

--- hardhat/hardhat-dao/contracts/governance_standard/TimLock.sol
+++ hardhat/hardhat-dao/contracts/governance_standard/TimLock.sol
@@ -1,5 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+
+	// minDelay: How long you have to wait before executing
+	// proposers is the list of the address that can propose
+	// executors: Who can execute a proposal passes
+	constructor(
+		uint256 minDelay,
+		address[] memory proposers,
+		address[] memory executors
+	) TimelockController(minDelay,proposers,executors) {}
+}
 // Que // we want to wait for a new vote to be "executed"
 
 // Everyone who holds the governance token has to pay 5 tokens
 
 // Give time to users to "get out" if they dont like a governance update
+
